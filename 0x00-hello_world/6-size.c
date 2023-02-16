d8172a3c0a88e511cac4451c268e6974ee03d03a
Fridausi <Frida>
2023-02-16 09:02:45 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing types sizes
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n");
+	printf("Size of an int: %lu byte(s)\n");
+	printf("Size of a long int: %lu byte(s)\n");
+	printf("Size of a long long int: %lu byte(s)\n");
+	printf("Size of a float: %lu byte(s)\n");
+	return (0);
+}
