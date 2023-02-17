3518bea7c0e066b146a77c8c171bf4b77fba1e46
Fridausi <Frida>
2023-02-17 08:59:41 -0800

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,8 +6,12 @@
  */
 int main(void)
 {
-	char b;
-	for(b ='a'; b <= 'z'; ++b)
-		putchar("%b" , b);
-	return(0);
+	char b= 'a';
+	while(b <= 'z')
+	{
+		putchar(b);
+		b++
+	}
+	putchar("\n");
+	return (0);
 }
