2417231c834903360ba60596e465ab3ef265fc0c
Fridausi <Frida>
2023-02-17 15:49:08 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int a= 0;
+
+	for(a; a <= 9; a++)
+		putchar("%d", a);
+	putchar('\n');
+	return (0);
+}
