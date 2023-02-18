11dbf64028359211bfcbb3cbaaae76889debc634
Fridausi <Frida>
2023-02-17 16:46:26 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char a = '0';
+	char b = 'a';
+
+	while (a < 10)
+	{
+		putchar(a + 48, a);
+		a++;
+	}
+
+	while (b <= f)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
