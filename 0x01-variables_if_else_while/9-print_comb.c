da7b7e62f6d7d49cb5873f3d267cd40a166738fb
Fridausi <Frida>
2023-02-17 17:55:03 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
-	int a;
+	int a = 0;
 	
-	for (a = '0'; a < 10; a++)
+	while (a < 10)
 	{
-		putchar(a + '0');
-		if (a < 9)
+		putchar(a + 48);
+		if (a != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+		a++;
 	}
 	putchar('\n');
 	return (0);
