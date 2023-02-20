cad6f75ad16acd50fe08932ff85ef7974b7ec616
Fridausi <Frida>
2023-02-20 14:49:34 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,41 +7,41 @@
  */
 int main(void)
 {
-    int c = 0;
-    int c2;
-    int f_d, l_d, f_d2, l_d2;
-    int first_pair = 1;
+	int c = 0;
+	int c2;
+	int f_d, l_d, f_d2, l_d2;
+	int first_pair = 1;
 
-    while (c <= 98)
-    {
-        f_d = c / 10;
-        l_d = c % 10;
-        c2 = c + 1;
+	while (c <= 98)
+	{
+		f_d = c / 10;
+		l_d = c % 10;
+		c2 = c + 1;
 
-        while (c2 <= 99)
-        {
-            f_d2 = c2 / 10;
-            l_d2 = c2 % 10;
+		while (c2 <= 99)
+		{
+			f_d2 = c2 / 10;
+			l_d2 = c2 % 10;
+			
+			if (!first_pair)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(f_d + '0');
+			putchar(l_d + '0');
+			putchar(' ');
+			putchar(f_d2 + '0');
+			putchar(l_d2 + '0');
 
-            if (!first_pair)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-            putchar(f_d + '0');
-            putchar(l_d + '0');
-            putchar(' ');
-            putchar(f_d2 + '0');
-            putchar(l_d2 + '0');
+			first_pair = 0;
 
-            first_pair = 0;
+			c2++;
+		}
 
-            c2++;
-        }
+		c++;
+	}
 
-        c++;
-    }
-
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
