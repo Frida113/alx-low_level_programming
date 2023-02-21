27eaca785394ef5f9c930e6d0635c7a52d17ffaf
Fridausi <Frida>
2023-02-21 09:38:04 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lower case
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	int ten;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+			_putchar('\n');
+		}
+	}
+}
