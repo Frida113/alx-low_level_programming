8c8781ab5c167dfb4a49ac5dde9f3904d53a7848
Fridausi <Frida>
2023-02-22 03:22:23 -0800

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,6 @@ void print_times_table(int n)
 		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
-			_putchar(' ');
 
 			for (mult = 1; mult <= n; mult++)
 			{
@@ -37,6 +36,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 				_putchar((prod % 10) + '0');
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
