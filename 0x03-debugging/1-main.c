ca9906ce2a470162dc3af6cd6f11fc8952091d7b
Fridausi <Frida>
2023-02-23 19:04:09 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
 /**
-* main - avoids an infinite loop
-* Return: 0
-*/
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+**/
 
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 
 	/*
 	 * while (i < 10)
 	 * {
-	 *        putchar(i);
+	*	putchar(i);
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
