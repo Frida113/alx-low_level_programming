868e06fe4982f602499f2ab8b708ba34951b4689
Fridausi <Frida>
2023-02-24 16:01:30 -0800

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,19 +9,20 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-		else
-		{
-			int i, j;
+	}
 
-			for (i = 0; i < n; i++)
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
 			{
-				for (j = 0; j < i; j++)
-				{
-					_putchar(' ');
-				}
-				_putchar('\\');
-				_putchar('\n');
+				_putchar(' ');
 			}
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
 }
