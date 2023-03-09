4d4b816530e98790a2ba809f6139079bd35043c5
Fridausi <Frida>
2023-03-08 16:22:31 -0800

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,2 +1,15 @@
 #include "main.h"
-
+/**
+ * _strlen_recursion - writes the lenght of a string
+ * @s: the string
+ *
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursions(s + 1));
+	}
+	return (0);
+}
