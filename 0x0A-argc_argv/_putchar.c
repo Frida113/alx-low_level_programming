4492bba62cb952c920f8995368a4d2cb4dacb0d9
Fridausi <Frida>
2023-03-11 15:59:09 -0800

    compilations

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints the character c
+ * @c: the character to be printed
+ *
+ * Return: 1 on success
+ * -1 on error and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
