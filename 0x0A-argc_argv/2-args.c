1d14779b4e3fae2ca653d020db2547808dc73208
Fridausi <Frida>
2023-03-11 16:05:28 -0800

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints arguments it receives
- * @ardc: number of arguments
+ * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: 0
@@ -12,7 +12,7 @@ int main(int  argc, char *argv[])
 {
 	int i;
 
-	for (i - 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
