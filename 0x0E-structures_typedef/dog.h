450233d21eb989f19cac11be4078fc76e3c73108
Fridausi <Frida>
2023-03-20 19:05:21 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the struct type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+#endif
