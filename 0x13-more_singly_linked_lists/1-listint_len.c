07f0f4458be905531a68476d0f19925bd9cc00b1
Fridausi <Frida>
2023-04-04 19:58:50 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elewments
+ * @h: the element
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++
+	}
+
+	return (count);
+
+}
