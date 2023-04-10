fb8c184482a90e054fa37269fe913ef32eebaa06
Fridausi <Frida>
2023-04-09 18:35:57 -0700

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,21 +3,19 @@
 
 /**
  * get_bit - returns the value of a bit at a given index
- * @n: the value
+ * @n: the value of the bit
  * @index: the index
- * Return: the valie of the bit at index or
+ * Return: the value of the bit at index or
  * -1 if an error occured
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_value = (n >> index) & 1;
-	{
-		return (bit_value);
-	}
-
-	if (index >= sizeof(unsigned long int) * 8)
-	{
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	}
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
