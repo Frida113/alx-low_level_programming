003a00f38349d780f650745b62b10aaed9671032
Fridausi <Frida>
2023-04-10 17:28:50 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,16 +11,12 @@
  * error_exit - prints error message and exits with given exit code
  * @exit_code: exit code
  * @format: format of error messge
- * @...: additional arguments for the format
+ * @arg: additional arguments for the format
  */
 
-void error_exit(int exit_code, const char *format, ...)
+void error_exit(int exit_code, const char *format, *arg)
 {
-	va_list args;
-	va_start(args, format);
-	dprintf(STDERR_FILENO, "Error: ");
-	vdprintf(STDERR_FILENO, format, args);
-	va_end(args);
+	dprintf(STDERR_FILENO, format, arg);
 	exit(exit_code);
 }
 
@@ -35,37 +31,49 @@ int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, rwr;
 	char buffer[BUFFER_SIZE];
-	ssize_t read_bytes;
+	ssize_t read_bytes, nchars;
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n");
-
-	fd_from = open(argv[1], O_RDONLY);
-
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-	if (fd_to = -1)
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-
-	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		rwr = write(fd_to, buffer, read_bytes);
-
-		if (rwr != read_bytes)
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		error_exit(97, "Usage: cp file_from file_to\n");
 	}
 
-	if (read_bytes == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+
+	if (fd_from == -1 || fd_to == -1)
+	{
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
 
-	if (close(fd_from) == -1)
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(fd_from, buffer, 1024);
+		if (nchars == -1)
+		{
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+		}
+		read_bytes = write(fd_to, buffer, nchars);
+		if (read_bytes == -1)
+		{
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	rwr = close(fd_from);
+	if (rwr == -1)
+	{
 		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+	}
 
-	if (close(fd_to) == -1)
+	rwr = close(fd_to);
+	if (rwr == -1)
+	{
 		error_exit(100, "Error: Can't close fd %d\n", fd_to);
+	}
 
 	return (0);
 }
